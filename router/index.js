541bb8df7eff12ce98c9b229e9f265efafebbc3b
bonini81 <andresdominguez81@gmail.com>
2020-09-09 21:45:15 -0500

    Second refactor on the Joi library

--- router/index.js
+++ router/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { verifyToken }  = require('../middlewares');
-const {errors} = require('celebrate');
+
 
 
 //router.use(require('./LibrosRoutes'));
@@ -10,7 +10,7 @@ router.use(require('./PublicRoutes'));
 router.use(verifyToken);
 router.use(require('./PrivateRoutes')); 
 
-router.use(errors());
+
 
 
 module.exports = router;
