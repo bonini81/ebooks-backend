96e1071e3948cd58083628f219ea0dd8af081139
bonini81 <andresdominguez81@gmail.com>
2020-10-16 09:37:29 -0500

    Cloudinary Upload Endpoint working, finally

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const server = express();
 const fileUpload = require('express-fileupload');
+const cloudinary = require('cloudinary').v2;
 const cors = require('cors');
 const PORT = process.env.PORT || 3000;
-const {errors} = require('celebrate');
+const { errors } = require('celebrate');
 
 
 server.use(express.urlencoded({ extended: true }));
@@ -14,16 +15,43 @@ server.use(fileUpload({
 }));
 
 
-//Para que todos los endpoints puedan usar CORS, y sea mas seguro y heroku te deje hacerlo.
-server.use(cors());
+cloudinary.config ({
+    
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+
+});
 
 //File Upload End Point
-server.post('/upload', (req, res)=>  {
+server.post('/upload', (req, res)=>{
 
-    console.log(req.files.foto);
-    res.send('Esto es una prueba.');
-    
+    const file = req.files.photo;
+    console.log("File Uploaded Madafaka!", file);
+
+    cloudinary.uploader.upload(file.tempFilePath, function(error, result) {
+
+        if(error)  {
+            console.log(error);
+            res.status(400).json(error);
+        }
+
+        if (result)  {
+            console.log(result);
+            console.log(result.url)
+            res.status(200).json(result);
+        }
     });
+});
+
+   // res.status(200).json('Cool!');
+    
+
+
+
+
+//Para que todos los endpoints puedan usar CORS, y sea mas seguro y heroku te deje hacerlo.
+server.use(cors());
 
 // Initial Endpoit
 server.get('/', (req, res) => res.send('Hello World! Welcome to my Ebook World, Madafaka! Coño de tu manga'));
