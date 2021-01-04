c31a3c819a654486ef3f172548c88f0c932b9466
bonini81 <andresdominguez81@gmail.com>
2021-01-03 21:20:45 -0500

    Refactoring again the CORS origin URL

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ server.use(cors());
 //Testing my CORS hypothesis bug solution
 server.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000/upebook"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, Authorization, X-Requested-With, Content-Type, Accept");
 
     next();
