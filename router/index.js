b64c15f14b4222f93d0a7fce975775cb2952e43a
bonini81 <andresdominguez81@gmail.com>
2021-01-04 10:06:18 -0500

    Refactor Cors bug again

--- router/index.js
+++ router/index.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const { verifyToken }  = require('../middlewares');
+const server = express();
+const cors = require('cors');
 
+
+
+//Para que todos los endpoints puedan usar CORS, y sea mas seguro y heroku te deje hacerlo.
+server.use(cors());
+
+//Testing my CORS hypothesis bug solution
+server.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000/"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Authorization");
+
+    next();
+  });
 
 
 //router.use(require('./LibrosRoutes'));
--- server/index.js
+++ server/index.js
@@ -22,14 +22,7 @@ server.use(fileUpload({
 //Para que todos los endpoints puedan usar CORS, y sea mas seguro y heroku te deje hacerlo.
 server.use(cors());
 
-//Testing my CORS hypothesis bug solution
-server.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000/"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Headers", "Authorization");
 
-    next();
-  });
 
 
 // Initial Endpoit
