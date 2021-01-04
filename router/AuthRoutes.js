0525a6a05463007bc0b7f0d9991c43df73c02c82
bonini81 <andresdominguez81@gmail.com>
2021-01-03 19:01:49 -0500

    Added libros route on the auth routes therefore not token protected

--- router/AuthRoutes.js
+++ router/AuthRoutes.js
@@ -2,12 +2,12 @@
 const express = require('express');
 const router = express.Router();
 const {UsersController} = require('../controller');
-
+const {LibrosController} = require('../controller');
 
 //Routes for SignUp Form and not Protected via Token, token not needed here
 router.post('/users/signup', UsersController.signup);
 router.post('/users/login', UsersController.login);
-
+router.post('/libros', LibrosController.create  );
 
 
 
