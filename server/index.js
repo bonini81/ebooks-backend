70d1c55458f1f1f544b097c131689f46453b63bf
bonini81 <andresdominguez81@gmail.com>
2021-01-05 10:33:23 -0500

    refactor test on CORS bug

--- server/index.js
+++ server/index.js
@@ -24,11 +24,10 @@ server.use(cors());
 //Testing my CORS hypothesis bug solution
 server.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Headers", "Authorization");
+    //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    //res.header("Access-Control-Allow-Headers", "Authorization");
     next();
   });
-
 
 
 // Initial Endpoit
